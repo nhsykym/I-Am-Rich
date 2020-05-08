ea4dadca5614feda837aeb256cd256f105869a84
Naohisa Yokoyama <nhsykym@gmail.com>
2020-05-08 15:24:58 +0900

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by 横山尚久 on 2020/05/08.
+//  Copyright © 2020 横山尚久. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
